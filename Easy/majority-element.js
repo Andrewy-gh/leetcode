83a8d31e4e0a27ecf70acfde307b5985834754fa
Andy-git985 <andy.yu617@gmail.com>
2022-12-13 18:25:08 -0500

    Majority Element sort solution

--- Easy/majority-element.js
+++ Easy/majority-element.js
@@ -57,6 +57,11 @@ var majorityElement = function (nums) {
   return candidate;
 };
 
+var majorityElement = function (nums) {
+  nums.sort();
+  return nums[Math.floor(nums.length / 2)];
+};
+
 nums = [3, 2, 3];
 console.log(majorityElement(nums)); // 3
 
