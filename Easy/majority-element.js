ef82ab77fd63694a25917ea8248ed130f011b333
Andy-git985 <andy.yu617@gmail.com>
2022-12-13 18:15:54 -0500

    Majority Element voting algorithm

--- Easy/majority-element.js
+++ Easy/majority-element.js
@@ -15,42 +15,53 @@
 // Input: nums = [2,2,1,1,1,2,2]
 // Output: 2
 
- 
-
 // Constraints:
 
 //     n == nums.length
 //     1 <= n <= 5 * 104
 //     -109 <= nums[i] <= 109
 
- 
 // Follow-up: Could you solve the problem in linear time and in O(1) space?
 
-var majorityElement = function(nums) {
-  const cache = {}
-  let max = 0
-  let val
-  let key
+var majorityElement = function (nums) {
+  const cache = {};
+  let max = 0;
+  let val;
+  let key;
   for (const num of nums) {
     if (cache[num]) {
-      val = ++cache[num]
+      val = ++cache[num];
     } else {
-      cache[num] = 1
-      val = 1
+      cache[num] = 1;
+      val = 1;
     }
     if (val > max) {
-      max = val
-      key = num
+      max = val;
+      key = num;
     }
   }
-  return Number(key)
+  return Number(key);
 };
 
-nums = [3, 2, 3]
-console.log(majorityElement(nums)) // 3
+var majorityElement = function (nums) {
+  let count = 0;
+  let candidate = null;
+  for (const num of nums) {
+    if (count === 0) candidate = num;
+    if (num === candidate) {
+      count += 1;
+    } else {
+      count -= 1;
+    }
+  }
+  return candidate;
+};
 
-nums = [2, 2, 1, 1, 1, 2, 2]
-console.log(majorityElement(nums)) // 2
+nums = [3, 2, 3];
+console.log(majorityElement(nums)); // 3
 
-nums = [1]
-console.log(majorityElement(nums)) // 1
+nums = [2, 2, 1, 1, 1, 2, 2];
+console.log(majorityElement(nums)); // 2
+
+nums = [1];
+console.log(majorityElement(nums)); // 1
