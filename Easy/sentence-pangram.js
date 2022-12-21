38b79bc5aba83d75558e9b0ad3e80ad517fa5e67
Andy-git985 <andy.yu617@gmail.com>
2022-12-21 15:54:31 +0000

    1832. Check if the Sentence Is Pangram

---
+++ Easy/sentence-pangram.js
@@ -0,0 +1,48 @@
+// 1832. Check if the Sentence Is Pangram
+// Easy
+
+// A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+// Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+// Output: true
+// Explanation: sentence contains at least one of every letter of the English alphabet.
+
+// Example 2:
+
+// Input: sentence = "leetcode"
+// Output: false
+
+ 
+
+// Constraints:
+
+//     1 <= sentence.length <= 1000
+//     sentence consists of lowercase English letters.
+
+
+var checkIfPangram = function(sentence) {
+    return new Set([...sentence]).size === 26
+};
+
+var checkIfPangram = function(sentence) {
+    const alphabet = 'abcdefghijklmnopqrstuvwzyz'
+    for (const letter of alphabet) {
+        if (!sentence.includes(letter)) {
+            return false
+        }
+    }
+    return true
+};
+
+
+sentence = "thequickbrownfoxjumpsoverthelazydog"
+console.log(checkIfPangram(sentence)) // true
+
+sentence = "leetcode"
+console.log(checkIfPangram(sentence)) // false
