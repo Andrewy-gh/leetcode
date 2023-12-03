5664ca2607ff9e05c6b1089ad79a4a159d621f63
Andrew <andy.yu617@gmail.com>
2023-12-03 12:44:56 -0500

    219. Contains Duplicate II

---
+++ Easy/contains-duplicates-ii.js
@@ -0,0 +1,50 @@
+// 219. Contains Duplicate II
+// Easy
+
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+
+// Constraints:
+
+//     1 <= nums.length <= 105
+//     -109 <= nums[i] <= 109
+//     0 <= k <= 105
+
+var containsNearbyDuplicate = function (nums, k) {
+  const window = new Set();
+  let l = 0;
+  for (let r = 0; r < nums.length; r++) {
+    if (r - l > k) {
+      window.delete(nums[l]);
+      l++;
+    }
+    if (window.has(nums[r])) {
+      return true;
+    }
+    window.add(nums[r]);
+  }
+  return false;
+};
+
+(nums = [1, 2, 3, 1]), (k = 3);
+console.log(containsNearbyDuplicate(nums, k)); // true
+
+(nums = [1, 0, 1, 1]), (k = 1);
+console.log(containsNearbyDuplicate(nums, k)); // true
+
+(nums = [1, 2, 3, 1, 2, 3]), (k = 2);
+console.log(containsNearbyDuplicate(nums, k)); // false
